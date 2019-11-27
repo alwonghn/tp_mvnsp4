0c0cfef470a38b6acc2ef0cb0452605ede4b721f
alwonghn <al.wong.hn@gmail.com>
2019-11-27 16:32:43 +0800

    remade to work in eclipse+maven+spring4

--- src/main/java/com/demo/controller/HelloWorldController.java
+++ src/main/java/com/demo/controller/HelloWorldController.java
@@ -4,21 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
+import org.springframework.web.servlet.ModelAndView;
+
+// @RequestMapping("/")
 @Controller
-@RequestMapping("/")
 public class HelloWorldController {
  
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
     public String sayHello(ModelMap model) {
+    	System.out.println("INTO SAYHELLO");
         model.addAttribute("greeting", "Hello World from Spring 4 MVC");
         return "welcome";
     }
+//	@RequestMapping(value="/hello", method = RequestMethod.GET)
+//    public ModelAndView sayHello() {
+//		System.out.println("INTO SAYHELLO");
+//    	String greeting="Hello World from Spring 4 MVC";
+//    	return new ModelAndView("welcome", "greeting",greeting);
+//    }
  
-    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
-    public String sayHelloAgain(ModelMap model) {
-        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-        return "welcome";
-    }
+//    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
+//    public String sayHelloAgain(ModelMap model) {
+//        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+//        return "welcome";
+//    }
  
 }
